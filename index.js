72e69673b429c6a14a9f7281aa927dc37b5f622f
Juan Andrés Nieto Gimenez <juandresnietog@gmail.com>
2024-07-11 10:51:03 -0300

    puerto

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("puntoverdebackend!!");
 });
 
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
