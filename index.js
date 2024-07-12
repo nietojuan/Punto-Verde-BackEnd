96805a706076bda897afae118232882f72c709d9
Juan Andrés Nieto Gimenez <juandresnietog@gmail.com>
2024-07-12 08:49:54 -0300

    armado rutas

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const productosRouter = require("./routes/productos.routes");
+const categoriasRouter = require("./routes/categoria.routes");
+const sucursalesRouter = require("./routes/sucursales.routes");
+const contactoRouter = require("./routes/contacto.routes");
+
+app.use("/productos", productosRouter);
+app.use("/categoria", categoriasRouter);
+app.use("/sucursales", sucursalesRouter);
+app.use("/contacto", contactoRouter);
+
+// app.use("/productos", require("./routes/productos.routes"));
+// app.use("/usuarios", require("./routes/usuarios.routes"));
+// app.use("/categorias", require("./routes/categorias.routes"));
+// app.use("/datosClientes", require("./routes/catosClientes.routes"));
+
 app.get("/", (req, res) => {
   res.send("Punto Verde BackEnd!!");
 });
