12697be9388825b8962bedf717fa37b3dbc428aa
Juan Andrés Nieto Gimenez <92448201+nietojuan@users.noreply.github.com>
2024-07-12 15:56:31 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "juannieto.alwaysdata.net/",
-  user: process.env.DB_USER || "juannieto",
-  password: process.env.DB_PASSWORD || "loquillo",
-  database: process.env.DB_NAME || "juannieto_puntoverde_backend",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((error) => {
